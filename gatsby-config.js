2a2f6e0c81de073431a45a9a4ab6e26499605820
Andre Clark <abclark@cosmicdivision.design>
2019-07-24 15:37:59 -0700

    updated personal data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,145 +1,84 @@
-const tailwind = require('./tailwind.config.js');
-
-const { palette } = tailwind.theme;
-
 module.exports = {
-    siteMetadata: {
-        title: `Cosmic Division`,
-        author: `Andre Clark`,
-        authorBio: {
-            author: `Andre Clark`,
-            location: `Spokane, Washington`,
-        },
-        description: `A personal blog with tailwindcss`,
-        siteUrl: `https://cosmicdivision.dev`,
-        social: {
-            github: `AndreBClark`,
-        },
-    }, 
-    plugins: [
-        {
-            resolve: `gatsby-source-filesystem`,
+  siteMetadata: {
+    // edit below
+    title: `Cosmic Division`,
+    author: `Andre Clark`,
+    description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
+    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    social: {
+      twitter: ``,
+      github: `AndreBClark`,
+    },
+  },
+  plugins: [
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-feed`,
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
             options: {
-                path: `${__dirname}/content/blog`,
-                name: `blog`,
+              maxWidth: 590,
             },
-        },
-        {
-            resolve: `gatsby-source-sanity`,
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
             options: {
-                projectId: `vij17ctr`,
-                dataset: `production`,
-                // a token with read permissions is required
-                // if you have a private dataset
-                // token: process.env.SANITY_TOKEN,
-                // If the Sanity GraphQL API was deployed using `--tag <name>`,
-                // use `graphqlTag` to specify the tag name. Defaults to `default`.
-                graphqlTag: 'default',
+              wrapperStyle: `margin-bottom: 1.0725rem`,
             },
-        },
-          // ...
-        `gatsby-plugin-preact`,
-        `gatsby-plugin-offline`,
-        `gatsby-plugin-react-helmet`,
-        'gatsby-plugin-postcss',
-        `gatsby-plugin-emotion`,
-        `gatsby-plugin-netlify`,
-        `gatsby-plugin-image`,
-        {
-            resolve: `gatsby-plugin-sharp`,
-            options: {
-                defaults: {
-                formats: [`auto`, 'avif', 'webp',`png`],
-                    placeholder: `blurred`,
-                    quality: 75,
-                    breakpoints: [300, 750, 1080, 1366, 1920],
-                    backgroundColor: 'transparent',
-                    tracedSVGOptions: {
-                        blackOnWhite: false,
-                        backgroundColor: 'transparent'
-                    },
-                    blurredOptions: {
-                        backgroundColor: 'transparent'
-                    },
-                },
-            },
-        },
-        `gatsby-transformer-sharp`,
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `images`,
-                path: `${__dirname}/src/images`,
-            },
-        },
-        {
-            resolve: "gatsby-plugin-transition-link",
-            options: {
-                layout: require.resolve(`./src/components/Base.jsx`)
-            }
-        },
-        {
-            resolve: `gatsby-transformer-remark`,
-            options: {
-                plugins: [
-                    {
-                        resolve: `gatsby-remark-prismjs`,
-                        options: {
-                            inlineCodeMarker: '~',
-                        }
-                    },
-                    {
-                        resolve: `gatsby-remark-images`,
-                        options: {
-                            maxWidth: 768,
-                        },
-                    },
-                    {
-                        resolve: "gatsby-plugin-react-svg",
-                        options: {
-                            rule: {
-                                include: /\.svg$/
-                            },
-                        },
-                    },
-                ]
-            },
-        },
-        // {
-        //     resolve: "gatsby-plugin-google-tagmanager",
-        //     options: {
-        //         id: "GTM-KMKSS6R",
-        //         includeInDevelopment: false,
-        //         defaultDataLayer: { platform: "gatsby" },
-        //     }
-        // },
-        {
-            resolve: 'gatsby-plugin-module-resolver',
-            options: {
-                root: './src', // <- will be used as a root dir
-                aliases: {
-                    hooks: './hooks',
-                    components: './components',
-                    images: './images',
-                    pages: './pages',
-                    static: {
-                        root: './public', // <- will used as this alias' root dir
-                        alias: './static' // <- will become ./public/static
-                    }
-                }
-            }
-        },
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: `Cosmic Division Jamstack Blog`,
-                short_name: `CosmicDiv`,
-                start_url: `/`,
-                background_color: palette.deep[100],
-                theme_color: palette.duo[100],
-                display: `standalone`,
-                icon: `src/images/icons/logo.svg`,
-            },
-        },
-    ],
+          },
+          `gatsby-remark-vscode`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // edit below
+        // trackingId: `ADD YOUR TRACKING ID HERE`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Gatsby Starter Blog`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        // edit below
+        icon: `content/assets/gatsby-icon.png`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
 }
