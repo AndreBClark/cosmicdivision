ad402935d6cef0876c3db1b5bc850b12b5f848cc
Andre Clark <abclark@cosmicdivision.design>
2019-07-25 13:14:07 -0700

    Revert "removed text decoration from buttons"
    This reverts commit ae34393d9bc6cbd859e694cca3cfb9fcba46b7b1.

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,7 +36,7 @@ class IndexPage extends React.Component {
           default starter blog running on Netlify CMS.
         </p>
         <p>Now go build something great!</p>
-        <Link to="/blog/">
+        <Link to="/blog/" style={{textDecoration: 'none'}}>
           <Button marginTop="35px">Go to Blog</Button>
         </Link>
       </Layout>
