aa63f1c20480481b96e412406780c7c6a05958b5
Andre Clark <aclark@hagadonmedia.com>
2019-08-28 13:19:18 -0700

    Update Svglogo.jsx
    sort imports

--- src/components/Svglogo.jsx
+++ src/components/Svglogo.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import PropTypes from "prop-types";
-import styled from "styled-components";
+import React from "react"
+import PropTypes from "prop-types"
+import styled from "styled-components"
 
 const SVG = styled.svg`
   margin: 0 1rem;
