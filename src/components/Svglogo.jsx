df1392bfb4590693b9979dfbf80aa59028103ae9
Andre Clark <aclark@hagadonmedia.com>
2019-08-28 13:25:34 -0700

    format svgLogo

--- src/components/Svglogo.jsx
+++ src/components/Svglogo.jsx
@@ -3,8 +3,8 @@ import PropTypes from "prop-types"
 import styled from "styled-components"
 
 const SVG = styled.svg`
-  margin: 0 1rem;
-`;
+  margin: 0.5rem 1rem;
+`
 
 const SvgLogo = props => {
   return (
@@ -20,8 +20,8 @@ const SvgLogo = props => {
         <path d="M160 16l23 110-86 115 118 37 23 110-219-69-19-92L130 56l30-40zm158 355l-26-110 97-114-133-39L231 0l260 76 9 84-182 211z"/>
       </SVG>
     </>
-  );
-};
+  )
+}
 
 SvgLogo.propTypes = {
   fill: PropTypes.string,
