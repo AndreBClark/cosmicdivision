09ee1e389d3a955389925c605e600d25349213d1
@andrebclark <abclark@cosmicdivision.design>
2019-08-27 23:00:43 -0700

    Create Hero.jsx

---
+++ src/components/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+import theme from "../../config/theme";
+import { rhythm, scale } from "../../config/typography";
+
+const Container = styled.section`
+  background-color: ${theme.colors.primary};
+  width: 100%;
+  display: flex;
+  min-height: 50vh;
+`;
+
+const Hero = ({ children }) => {
+  return (
+    <Container>
+        {children}
+    </Container>
+  )
+}
+
+export default Hero
