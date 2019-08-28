b74065f4dc69ca951c5006fac76399bb52615ab5
@andrebclark <abclark@cosmicdivision.design>
2019-08-27 22:45:43 -0700

    Create Panel.jsx
    from beholder app

---
+++ src/components/Panel.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from 'styled-components'
+import theme from "../../config/theme";
+import { rhythm, scale } from "../../config/typography";
+
+const Container = styled.section`
+  background-color: ${theme.colors.overlay.middle};
+  padding: ${rhythm(1)};
+  border-radius: ${theme.layoutConstants.radius};
+  margin: 2rem auto;
+
+  max-width: ${rhythm(24)};
+`;
+
+const Panel = ({ children }) => {
+  return (
+    <Container>
+        {children}
+    </Container>
+  )
+}
+
+export default Panel
