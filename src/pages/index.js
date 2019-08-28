62f5fa8aff3d75da002182d3a0947492a7f5e343
Andre Clark <aclark@hagadonmedia.com>
2019-08-28 13:20:44 -0700

    implement svglogo into home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,13 +10,6 @@ import theme from "../../config/theme"
 import SvgLogo from "../components/Svglogo"
 
 
-const Logosvg = styled.img`
-  transition: all 0.4s ease-in-out;
-  &:hover {
-    transform: scale(1.1);
-  }
-`
-
 class IndexPage extends React.Component {
   render() {
     const siteTitle = `Cosmic Division`
@@ -28,11 +21,6 @@ class IndexPage extends React.Component {
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
         <Hero>
-        <Logosvg
-          style={{ margin: `0 auto` }}
-          src="./GatsbyScene.svg"
-          width="50%"
-        />
         <Panel>
         <h1>
           Hey people
@@ -50,6 +38,26 @@ class IndexPage extends React.Component {
           <Button marginTop="35px">Go to Blog</Button>
         </Link>
         </Panel>
+          <SvgLogo fill={"none"} stroke={theme.colors.primary} />
+          <Panel>
+            <h1>
+              Hey people
+              <span role="img" aria-label="wave emoji">
+                👋
+              </span>
+            </h1>
+            <p>
+              Welcome to your new Gatsby website. You are on your home page.
+            </p>
+            <p>
+              This starter comes out of the box with styled components and
+              Gatsby's default starter blog running on Netlify CMS.
+            </p>
+            <p>Now go build something great!</p>
+            <Link to="/blog/" style={{ textDecoration: "none" }}>
+              <Button marginTop="35px">Go to Blog</Button>
+            </Link>
+          </Panel>
         </Hero>
       </TemplateWrapper>
     )
