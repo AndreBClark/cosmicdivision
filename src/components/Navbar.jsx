bca2aa3849f0c0bd4209af6601b9ad2e0c46ae83
@andrebclark <abclark@cosmicdivision.design>
2019-08-27 23:00:37 -0700

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { Link } from "gatsby";
+import styled, { ThemeProvider } from "styled-components";
+import { rhythm } from "../../config/typography";
+import theme from "../../config/theme";
+import SvgLogo from "./Svglogo";
+
+const Header = styled.header`
+  font-size: 20px;
+  font-weight: 600;
+  background-color: ${props => props.theme.colors.primary};
+  li a {
+    color: ${props => props.theme.colors.bg};
+    margin: 10px 25px;
+  }
+`;
+
+const Nav = styled.nav`
+  max-width: 1280px;
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  @media screen and (min-width: 768px) {
+    justify-content: space-between;
+  }
+`;
+const List = styled.ul`
+  display: flex;
+  list-style-type: none;
+  margin: 0;
+  padding-left: 0;
+  li {
+    margin: 20px 25px;
+  }
+`;
+
+const Navbar = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Header>
+        <Nav>
+          <Link to="/" id="title" class="nav__item">
+            <SvgLogo fill={theme.colors.bg} />
+          </Link>
+          <List>
+            <li>
+              <Link to="blog">Blog</Link>
+            </li>
+            <li>
+              <Link to="bio">Bio</Link>
+            </li>
+          </List>
+        </Nav>
+      </Header>
+    </ThemeProvider>
+  );
+};
+
+export default Navbar;
