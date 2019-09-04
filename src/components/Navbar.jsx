9e20eb5cca4f5425ade8f0bd063d1fe6d04585ab
Andre Clark <aclark@hagadonmedia.com>
2019-09-04 14:03:30 -0700

    adjust svg logo
    Co-Authored-By: Andre <andre@cosmicdivision.dev>

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import theme from "../../config/theme"
 import SvgLogo from "./Svglogo"
 
 const Header = styled.header`
-  font-size: 20px;
+  font-size: 1rem;
   font-weight: 600;
   background-color: ${props => props.theme.colors.grey.default};
   li a {
@@ -15,13 +15,14 @@ const Header = styled.header`
 `
 
 const Nav = styled.nav`
-  max-width: 1280px;
+  max-width: ${theme.maxWidth};
   margin: 0 auto;
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
   @media screen and (min-width: 768px) {
     justify-content: space-between;
+    padding: 0;
   }
 `
 const List = styled.ul`
@@ -30,7 +31,7 @@ const List = styled.ul`
   margin: 0;
   padding-left: 0;
   li {
-    margin: 20px 25px;
+    margin: 1.5rem 0;
   }
 `
 
@@ -39,21 +40,20 @@ const Navbar = () => {
     <ThemeProvider theme={theme}>
       <Header>
         <Nav>
-          <Link to="/" id="title" class="nav__item">
+          <Link to="/" id="title">
             <SvgLogo
-              stroke={theme.colors.primary}
-              height={"50px"}
+              stroke={`none`}
+              height={"2rem"}
               strokeWidth={"10px"}
-              fill={`none`}
-              style={{ margin: "1rem" }}
+              fill={theme.colors.primary}
             />
           </Link>
           <List>
             <li>
-              <Link to="blog">Blog</Link>
+              <Link to="/blog">Blog</Link>
             </li>
             <li>
-              <Link to="bio">Bio</Link>
+              <Link to="/bio">Bio</Link>
             </li>
           </List>
         </Nav>
