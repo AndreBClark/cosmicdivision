09adcb37dbb5756734b9988683d07872a4309f1d
Andre Clark <aclark@hagadonmedia.com>
2019-09-04 10:02:11 -0700

    format

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,29 +1,19 @@
 import React from "react"
 import styled from "styled-components"
 import theme from "../../config/theme"
-import ContentContainer from "./ContentContainer"
 
 const Container = styled.section`
   background-color: ${theme.colors.grey.dark};
   width: 100%;
   min-height: 50vh;
-  padding: 2rem;
-`
-const FlexLg = styled.div`
+  padding: 1rem 0;
   @media screen and (min-width: 767px) {
     display: flex;
-    align-content: center;
-    align-items: center;
   }
 `
+
 const Hero = ({ children }) => {
-  return (
-    <Container>
-      <ContentContainer>
-        <FlexLg>{children}</FlexLg>
-      </ContentContainer>
-    </Container>
-  )
+  return <Container>{children}</Container>
 }
 
 export default Hero
