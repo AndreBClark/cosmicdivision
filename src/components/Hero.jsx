b2eb34b5ac7791a9dcedd53d0f183fe0d1b22144
Andre <andre@cosmicdivision.dev>
2019-09-18 00:16:09 -0700

    add background image to hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,12 +2,17 @@ import React from "react"
 import styled from "styled-components"
 import theme from "../../config/theme"
 import ContentContainer from "./ContentContainer"
+import Background from "../../content/assets/orbital-bw.jpg"
 
 const Container = styled.section`
   background-color: ${theme.colors.grey.dark};
   width: 100%;
   min-height: 50vh;
   padding: 0.25rem;
+  background-image: linear-gradient(rgba(0,0,0,0.25), rgba(0,0,0,0.25), rgba(0,0,0,0.25), rgba(0,0,0,1)),
+  url(${Background});
+  background-size: cover;
+  background-position: center center;
   @media screen and (min-width: 769px) {
     padding: 4rem;
   }
