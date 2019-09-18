c6e5ab6c4ab2e4bea03dce3abec75900d0e67c83
Andre <andre@cosmicdivision.dev>
2019-09-17 22:22:45 -0700

    add orbitron as heading

--- config/theme.js
+++ config/theme.js
@@ -30,6 +30,7 @@ const fontFamily = {
     mono: `'Fira Mono'`,
     serif: `'Bitter', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Helvetica', 'Arial', serif`,
     sans: `'Fira Sans', 'Open Sans', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Helvetica', 'Arial', sans-serif`,
+    heading: `'Orbitron'`
 }
 
 const breakpoints = {
