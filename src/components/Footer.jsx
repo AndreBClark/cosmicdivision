85af24b5781875d5336763722447f75853982ae7
Andre <andre@cosmicdivision.dev>
2019-09-18 00:15:31 -0700

    fit footer to bottom of page without vertically centering main content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,11 @@ import theme from "../../config/theme"
 const FooterSection = styled.footer`
   background-color: ${props => props.theme.colors.bg};
   padding: 2rem;
+  margin-top: auto;
 `
 const Inner = styled.div`
   margin: 0 auto;
+  font-size: .8rem;
   max-width: ${theme.maxWidth};
 `
 const Footer = () => {
