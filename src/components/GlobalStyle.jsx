ff24fc0efb615557b9c33ceb40d8c1c6837d2c4f
Andre <andre@cosmicdivision.dev>
2019-10-04 00:41:51 -0700

    migrate heading styling to theme.js
    Because:
    - typography.js is MEH

--- src/components/GlobalStyle.jsx
+++ src/components/GlobalStyle.jsx
@@ -12,6 +12,10 @@ body {
   margin: 0;
   font-size: ${theme.baseFontSize}
 }
+h1,h2,h3,h4,h5,h6 {
+  font-family: ${theme.fontFamily.heading};
+  font-weight: ${theme.fontFamily.headingWeight};
+}
 * {
   box-sizing: inherit;
 }
