a9f6b35396ba7be476ca29aa53b0aae152d087a2
Andre <andre@cosmicdivision.dev>
2019-10-04 00:41:19 -0700

    changed min-height
    Because:
    - to prevent overflow on short pages

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ import Background from "../images/orbital-bw.jpg"
 const Container = styled.section`
   background-color: ${theme.colors.grey.dark};
   width: 100%;
-  min-height: 84vh;
+  min-height: 75vh;
   padding: 0.25rem;
   text-align: center;
   background-image: linear-gradient(0deg, rgba(18,18,18,1) 5%, rgba(18,18,18,0) 25%, rgba(18,18,18,0) 50%),
@@ -18,6 +18,7 @@ const Container = styled.section`
   align-items: center;
   @media screen and (min-width: 769px) {
     padding: 4rem;
+    min-height: 80vh;
   }
 `
 const FlexLg = styled.div`
