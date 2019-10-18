39b290b8c0bcf0fc76c7bd84a2a3836d3120d525
@andrebclark <abclark@cosmicdivision.design>
2019-10-17 20:45:41 -0700

    add animation to panel
    Because:
    - it looks cool
    - feels nice to use

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -1,15 +1,26 @@
 import React from "react"
 import styled from "styled-components"
 import theme from "../../config/theme"
-import { rhythm } from "../../config/typography"
 
 const Container = styled.section`
   background-color: ${theme.colors.overlay.middle};
-  padding: ${theme.spacer};
-  border-radius: ${theme.radius};
-  margin: 1rem auto;
-  max-width: ${rhythm(24)};
+  padding: 4vmin;
+  margin: ${props => props.theme.spacer};
   height: 100%;
+  border-left: solid ${theme.stroke} ${props => props.theme.colors.primary};
+    ul {
+      margin-left: 1.5rem;
+    }
+  @media screen and (min-width: 760px) {
+    opacity: .7;
+    border-radius: ${theme.radius};
+    padding: 4vmin 6vmin;
+    transition: all ${props => props.theme.transitions.normal};
+    &:hover {
+      transform: translateY(-4px);
+      opacity: 1;
+    }
+  }
 `
 const Panel = ({ children }) => {
   return <Container>{children}</Container>
