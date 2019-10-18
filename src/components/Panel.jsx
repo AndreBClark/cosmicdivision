806cc51cf82dae3f3a3d4a7f0987de993a9248fc
@andrebclark <abclark@cosmicdivision.design>
2019-10-17 23:08:02 -0700

    add props.theme
    Because:
     - removes dependence on theme import and instead pulls in theme from props

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -1,21 +1,19 @@
 import React from "react"
 import styled from "styled-components"
-import theme from "../../config/theme"
 
 const Container = styled.section`
-  background-color: ${theme.colors.overlay.middle};
+  background-color: ${props => props.theme.colors.overlay.middle};
   padding: 4vmin;
   margin: ${props => props.theme.spacer};
   height: 100%;
-  border-left: solid ${theme.stroke} ${props => props.theme.colors.primary};
+  border-left: solid ${props => props.theme.stroke} ${props => props.theme.colors.primary};
     ul {
       margin-left: 1.5rem;
     }
   @media screen and (min-width: 760px) {
     opacity: .7;
-    border-radius: ${theme.radius};
+    border-radius: ${props => props.theme.radius};
     padding: 4vmin 6vmin;
-    transition: all ${props => props.theme.transitions.normal};
     &:hover {
       transform: translateY(-4px);
       opacity: 1;
