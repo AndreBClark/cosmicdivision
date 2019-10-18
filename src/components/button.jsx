1e8d57d7b485d1b695ad8cb2fbd686e4455995cc
@andrebclark <abclark@cosmicdivision.design>
2019-10-17 23:13:57 -0700

    Remove redundancies

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,11 +1,8 @@
 import React from "react"
-import styled, { ThemeProvider } from "styled-components"
-import theme from "../../config/theme"
+import styled from "styled-components"
 
 const Button = props => (
-  <ThemeProvider theme={theme}>
     <ButtonWrapper props={props}>{props.children}</ButtonWrapper>
-  </ThemeProvider>
 )
 
 const ButtonWrapper = styled.button`
@@ -15,17 +12,16 @@ const ButtonWrapper = styled.button`
   letter-spacing: 2px;
   background: none;
   border-color: ${props => props.theme.primary};
-  border: 3px solid;
   color: ${props => props.theme.colors.primary};
   font-weight: ${props => props.theme.fontWeight};
   border-radius: ${props => props.theme.radius};
   font-family: ${props => props.theme.fontFamily.heading};
 
   &:hover {
-    text-decoration: none !important;
     color: ${props => props.theme.colors.bg};
     background-color: ${props => props.theme.colors.primary};
     border-color: ${props => props.theme.colors.primary};
+      cursor: pointer !important;
   }
 `
 
