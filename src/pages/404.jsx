3af93f8ef5affda882e0af535beee3e76add4bc9
@andrebclark <abclark@cosmicdivision.design>
2019-10-30 22:28:09 -0700

    add home button for 404

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -3,8 +3,9 @@ import { graphql } from "gatsby"
 
 import TemplateWrapper from "../components/Base"
 import SEO from "../components/SEO"
-import ContentContainer from '../components/ContentContainer'
+import ContentContainer from "../components/ContentContainer"
 import Panel from "../components/Panel"
+import Button from "../components/button"
 
 class NotFoundPage extends React.Component {
   render() {
@@ -13,12 +14,13 @@ class NotFoundPage extends React.Component {
 
     return (
       <TemplateWrapper location={this.props.location} title={siteTitle}>
-          <ContentContainer>
-        <SEO title="404: Not Found" />
-        <h1>Not Found</h1>
-        <Panel>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-        </Panel>
+        <ContentContainer>
+          <SEO title="404: Not Found" />
+          <h1>Not Found</h1>
+          <Panel>
+            <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+            <Button>GO HOME</Button>
+          </Panel>
         </ContentContainer>
       </TemplateWrapper>
     )
