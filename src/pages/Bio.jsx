58d06788c7708c33240c860020f2ff379f193989
Andre <andre@cosmicdivision.dev>
2019-11-14 21:19:59 -0800

    Update Bio.jsx

--- src/pages/Bio.jsx
+++ src/pages/Bio.jsx
@@ -1,15 +1,15 @@
 import React from "react"
-import Layout from "../components/Base"
+import Base from "../components/Base"
 import BioCard from "../components/BioCard"
 import ContentContainer from "../components/ContentContainer"
 const Bio = () => {
   return (
-    <Layout>
+    <Base>
       <ContentContainer>
         <h1>About Andre</h1>
         <BioCard />
       </ContentContainer>
-    </Layout>
+    </Base>
   )
 }
 
