17a98427a75f0d4ad4b0240bb5774ed9b71b95ad
@andrebclark <abclark@cosmicdivision.design>
2019-11-16 23:09:20 -0800

    add tailwind heading tags

--- src/components/GlobalStyle.jsx
+++ src/components/GlobalStyle.jsx
@@ -1,11 +1,14 @@
 import { createGlobalStyle } from "styled-components"
 import theme from "../../config/theme"
+import tw from 'tailwind.macro'
 import Preflight from "./Preflight"
 
 const GlobalStyle = createGlobalStyle`
 html {
   min-height: 100%;
 ${Preflight}
+h1 {
+  ${tw`text-5xl`}
 }
 body {
   font-family: ${theme.fontFamily.sans};
@@ -14,6 +17,21 @@ body {
   margin: 0;
   font-size: ${theme.baseFontSize};
   line-height:1.6;
+h2 {
+  ${tw`text-3xl`}
+}
+h3 {
+  ${tw`text-2xl`}
+}
+h4 {
+  ${tw`text-xl`}
+}
+h5 {
+  ${tw`text-lg`}
+}
+h6 {
+  ${tw`text-base`}
+}
 }
 h1,h2,h3,h4,h5,h6 {
   font-family: ${theme.fontFamily.heading};
