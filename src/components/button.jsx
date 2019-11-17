fd2a3b7b4f4d94a3f96504261e1ab1acb282c3b7
@andrebclark <abclark@cosmicdivision.design>
2019-11-17 01:10:16 -0800

    Update button.jsx

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,21 +1,18 @@
-import React from "react"
 import styled from "styled-components"
+import { rhythm } from "../../config/typography"
+import { Link } from 'gatsby'
 
-const Button = props => (
-  <OutlineButton {...props} onclick={props}>
-    {props.children}
-  </OutlineButton>
-)
 
-const OutlineButton = styled.button`
-  padding: 0.5em 1.5em;
+export const OutlineLink = styled(Link)`
+  padding: ${[rhythm(1 / 3), " ", rhythm(1)]};
+  margin: ${rhythm(1 / 3)};
   text-transform: ${props => props.theme.fontFamily.logoCase};
   letter-spacing: ${props => props.theme.letterSpacing};
-  background: none;
+  background-color: none;
   border: ${props => props.theme.border} solid;
   border-color: ${props => props.theme.primary};
   color: ${props => props.theme.colors.primary};
-  font-weight: ${props => props.theme.fontWeight};
+  font-weight: ${props => props.theme.fontFamily.headingWeight};
   border-radius: ${props => props.theme.radius};
   font-family: ${props => props.theme.fontFamily.heading};
 
@@ -26,13 +23,72 @@ const OutlineButton = styled.button`
     cursor: pointer !important;
   }
 `
-export const SolidButton = styled(OutlineButton)`
-  background: ${props => props.theme.colors.primary};
+
+export const SolidLink = styled(OutlineLink)`
+  background-color: ${props => props.theme.colors.primary};
   color: ${props => props.theme.colors.grey.default};
   border-color: ${props => props.theme.colors.primary};
   &:hover {
-    color: ${props => props.theme.colors.bg};
-    background-color: ${props => props.theme.colors.primary};
+    color: ${props => props.theme.colors.primary};
+    background-color: transparent;
   }`
 
-export default Button
+export const OutlineBtn = styled.button`
+  padding: ${[rhythm(1 / 3), " ", rhythm(1)]};
+  margin: ${rhythm(1 / 3)};
+  text-transform: ${props => props.theme.fontFamily.logoCase};
+  letter-spacing: ${props => props.theme.letterSpacing};
+  background-color: none;
+  border: ${props => props.theme.border} solid;
+  border-color: ${props => props.theme.primary};
+  color: ${props => props.theme.colors.primary};
+  font-weight: ${props => props.theme.fontFamily.headingWeight};
+  border-radius: ${props => props.theme.radius};
+  font-family: ${props => props.theme.fontFamily.heading};
+
+  &:hover {
+    color: ${props => props.theme.colors.bg};
+    background-color: ${props => props.theme.colors.primary};
+    border-color: ${props => props.theme.colors.primary};
+    cursor: pointer !important;
+  }
+`
+
+export const SolidBtn = styled(OutlineBtn)`
+  background-color: ${props => props.theme.colors.primary};
+  color: ${props => props.theme.colors.grey.default};
+  border-color: ${props => props.theme.colors.primary};
+  &:hover {
+    color: ${props => props.theme.colors.primary};
+    background-color: transparent;
+  }`
+
+export const OutlineAnchor = styled.a`
+  padding: ${[rhythm(1 / 3), " ", rhythm(1)]};
+  margin: ${rhythm(1 / 3)};
+  text-transform: ${props => props.theme.fontFamily.logoCase};
+  letter-spacing: ${props => props.theme.letterSpacing};
+  background-color: none;
+  border: ${props => props.theme.border} solid;
+  border-color: ${props => props.theme.primary};
+  color: ${props => props.theme.colors.primary};
+  font-weight: ${props => props.theme.fontFamily.headingWeight};
+  border-radius: ${props => props.theme.radius};
+  font-family: ${props => props.theme.fontFamily.heading};
+
+  &:hover {
+    color: ${props => props.theme.colors.bg};
+    background-color: ${props => props.theme.colors.primary};
+    border-color: ${props => props.theme.colors.primary};
+    cursor: pointer !important;
+  }
+`
+
+export const SolidAnchor = styled(OutlineBtn)`
+  background-color: ${props => props.theme.colors.primary};
+  color: ${props => props.theme.colors.grey.default};
+  border-color: ${props => props.theme.colors.primary};
+  &:hover {
+    color: ${props => props.theme.colors.primary};
+    background-color: transparent;
+  }`
