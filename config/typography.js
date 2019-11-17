3013136bde96a776a2692b7b8e9ed754ab170d97
@andrebclark <abclark@cosmicdivision.design>
2019-11-16 21:44:28 -0800

    Update typography.js

--- config/typography.js
+++ config/typography.js
@@ -26,20 +26,6 @@ function _interopRequireDefault(obj) {
     }
 }
 
-function _defineProperty(obj, key, value) {
-    if (key in obj) {
-        Object.defineProperty(obj, key, {
-            value: value,
-            enumerable: true,
-            configurable: true,
-            writable: true,
-        })
-    } else {
-        obj[key] = value
-    }
-    return obj
-}
-
 const typography = new Typography({
         title: "CosmicDivision",
         baseLineHeight: 1.6,
