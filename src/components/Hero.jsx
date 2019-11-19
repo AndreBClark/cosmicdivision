8b05c78e500632dc1a11b6c0106a4520e4d6e288
@andrebclark <abclark@cosmicdivision.design>
2019-11-18 21:07:24 -0800

    added gatsby-background-image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,17 @@
 import React from 'react'
+import { graphql, StaticQuery } from 'gatsby'
 import styled from 'styled-components'
 import theme from '../../config/theme'
 import ContentContainer from './ContentContainer'
 import Background from '../images/orbital-bw.jpg'
+import BackgroundImage from 'gatsby-background-image'
 
-const Container = styled.section`
+
+const Container = styled(BackgroundImage)`
   background-color: ${theme.colors.grey.dark};
   width: 100%;
   min-height: 75vh;
   text-align: center;
-  background-image: linear-gradient(
-      0deg,
-      rgba(18, 18, 18, 1) 5%,
-      rgba(18, 18, 18, 0) 25%,
-      rgba(18, 18, 18, 0) 50%
-    ),
-    url(${Background});
   background-size: cover;
   background-position: center center;
   display: flex;
@@ -33,14 +29,41 @@ const FlexLg = styled.div`
     justify-content: space-between;
   }
 `
-const Hero = ({ children }) => {
-  return (
-    <Container>
+const Hero = ({ children }) => (
+  <StaticQuery
+  query={graphql`
+      query {
+        desktop: file(relativePath: { eq: "orbital-bw.jpg" }) {
+          childImageSharp {
+            fluid(quality: 90, maxWidth: 1920) {
+              ...GatsbyImageSharpFluid_withWebp
+              ...GatsbyImageSharpFluid_tracedSVG
+            }
+          }
+        }
+      }
+    `}
+    render={data => {
+      // Set ImageData.
+      const imageData = [
+        data.desktop.childImageSharp.fluid,
+        `linear-gradient(0deg, rgba(18, 18, 18, 1) 5%, rgba(18, 18, 18, 0) 25%, rgba(18, 18, 18, 0) 50%)` 
+      ].reverse()
+      return (
+        <Container
+        Tag="section"
+        fluid={imageData}
+        backgroundColor={`#040e18`}
+        >
       <ContentContainer>
         <FlexLg>{children}</FlexLg>
       </ContentContainer>
     </Container>
   )
 }
+}
+/>
+)
+
 
 export default Hero
