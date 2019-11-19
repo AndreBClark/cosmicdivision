9f1996e64b704e15e00ada522a6d6ad2c7f42e63
@andrebclark <abclark@cosmicdivision.design>
2019-11-18 23:59:40 -0800

    add polished to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
+const tint = require('polished/lib/color/tint');
+const shade = require('polished/lib/color/shade');
 module.exports = {
     theme: {
         colors: {
-            primary: '#26ffba'
+            primary: '#26ffba',
+            gray: {
+                900: `${shade(.7, "#121212")}`,
+                800: `${shade(.5, "#121212")}`,
+                700: `${shade(.4, "#121212")}`,
+                600: `${shade(.3, "#121212")}`,
+                500: '#121212',
+                400: `${tint(0.1, "#121212")}`,
+                300: `${tint(0.2, "#121212")}`,
+                200: `${tint(0.3, "#121212")}`,
+                100: `${tint(0.4, "#121212")}`
+            }
         }
     },
     variants: {},
