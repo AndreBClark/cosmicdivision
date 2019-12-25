faa27eb2d951e7ab4185b18e1fc09427db543d8f
Andre <andre@cosmicdivision.dev>
2019-12-24 20:51:27 -0800

    Revert ":heavy_minus_sign: DEP remove feed"
    This reverts commit e2f1e4c9e992a3bc3605f712bf74108c4a669414.

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,7 @@ module.exports = {
                 displayName: true,
             },
         },
+        `gatsby-plugin-feed`,
         `gatsby-plugin-offline`,
         `gatsby-plugin-react-helmet`,
         `gatsby-background-image`,
