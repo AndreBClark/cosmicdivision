9c3c40a10fb1ecb7b87d1cd14b582f9d2826f800
AndreBClark <andre@cosmicdivision.dev>
2020-02-16 19:14:34 -0800

    :recycle: resize images

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -5,59 +5,64 @@ import { SmBtnOutline, SmBtn } from '../components/button'
 import tw from 'tailwind.macro'
 import SEO from '../components/SEO'
 import styled from 'styled-components'
-import Img from "gatsby-image";
+import Img from 'gatsby-image'
 import { useStaticQuery, graphql } from 'gatsby'
 const Flex = styled.div`
-  ${tw`flex w-full`}
+@media screen and (min-width: ${props => props.theme.breakpoints.desktop}){
+  ${tw`flex max-w-full`}
+}
 `
 const ButtonContainer = styled(Flex)`
-  ${tw`justify-between w-56 max-w-full`}
+  ${tw`justify-between w-56`}
 `
 const Item = styled(Flex)`
   border-bottom-width: 4px;
-  ${tw`w-full pb-6 mb-8 border-primary`}
-  & p {
-    ${tw`py-3`}
-  }
+  ${tw`justify-between w-full pb-6 mb-8 border-primary`}
+
 `
 
 const Thumbnail = styled(Img)`
   ${tw`w-full p-4 rounded-lg`}
   flex: 1 0 15%;
-  
+  img {
+    object-position: 0 center !important;
+  }
 `
-const Description = styled.div`
+const Description = styled(Flex)`
   flex: 0 5 85%;
-  ${tw`content-start w-full px-4`}
+  ${tw`flex-col content-start justify-between w-full px-4`}
   h2 {
     ${tw`-mt-1`}
   }
+  p {
+    ${tw`py-3`}
+  }
 `
 const Projects = ({ props }) => {
-  const { dscvr, companiondice, dribbble01 }= useStaticQuery(graphql`
-  query {
-    dscvr: file(relativePath: { eq: "dscvr.png" }) {
-      childImageSharp {
-        fluid(quality: 100, maxWidth: 412) {
-          ...GatsbyImageSharpFluid_withWebp
+  const { dscvr, companiondice, dribbble01 } = useStaticQuery(graphql`
+    query {
+      dscvr: file(relativePath: { eq: "dscvr.png" }) {
+        childImageSharp {
+          fluid(quality: 100, maxWidth: 412) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+      companiondice: file(relativePath: { eq: "companiondice.png" }) {
+        childImageSharp {
+          fluid(quality: 100, maxWidth: 412) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+      dribbble01: file(relativePath: { eq: "dribbble01.png" }) {
+        childImageSharp {
+          fluid(quality: 100, maxWidth: 412) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
         }
       }
     }
-    companiondice: file(relativePath: { eq: "companiondice.png" }) {
-      childImageSharp {
-        fluid(quality: 100, maxWidth: 412) {
-          ...GatsbyImageSharpFluid_withWebp
-        }
-      }
-    }
-  dribbble01: file(relativePath: { eq: "dribbble01.png" }) {
-      childImageSharp {
-        fluid(quality: 100, maxWidth: 412) {
-          ...GatsbyImageSharpFluid_withWebp
-        }
-      }
-    }
-  }
   `)
   return (
     <>
@@ -69,80 +74,84 @@ const Projects = ({ props }) => {
         <h1>Projects</h1>
         <Panel>
           <Item>
-          <Thumbnail
+            <Thumbnail
               fluid={companiondice.childImageSharp.fluid}
               alt="dscvr project"
-              />
-          <Description >
+            />
+            <Description>
               <h2>Companion Dice</h2>
               <p>Dice Rolling app for Playing Table Top Games</p>
-            <ButtonContainer>
-              <SmBtn
-                as="a"
-                target="_blank"
-                href="https://companiondice.netlify.com/">
-                Live Demo
-              </SmBtn>
-              <SmBtnOutline
-                as="a"
-                target="_blank"
-                href="https://github.com/AndreBClark/companiondice">
-                Github Repo
-              </SmBtnOutline>
-            </ButtonContainer>
-            <hr />
-          </Description>
+              <ButtonContainer>
+                <SmBtn
+                  as="a"
+                  target="_blank"
+                  href="https://companiondice.netlify.com/">
+                  Live Demo
+                </SmBtn>
+                <SmBtnOutline
+                  as="a"
+                  target="_blank"
+                  href="https://github.com/AndreBClark/companiondice">
+                  Github Repo
+                </SmBtnOutline>
+              </ButtonContainer>
+              <hr />
+            </Description>
           </Item>
           <Item>
-          <Thumbnail
+            <Thumbnail
               fluid={dribbble01.childImageSharp.fluid}
-              alt="dribbble Challenge" />
-          <Description>
-            <h2>Real Estate Dribbble sprint</h2>
-            <p>
-              One Day project to recreate a website mockup from a Dribbble Shot
-            </p>
-            <ButtonContainer>
-              <SmBtn
-                href="https://dribbblechallenge-realestate.netlify.com/"
-                as="a">
-                Live Demo
-              </SmBtn>
-              <SmBtnOutline
-                as="a"
-                href="https://github.com/AndreBClark/dribbble-realestate">
-                Github Repo
-              </SmBtnOutline>
-            </ButtonContainer>
-            <hr />
-              </Description>
+              alt="dribbble Challenge"
+            />
+            <Description>
+              <h2>Real Estate Dribbble sprint</h2>
+              <p>
+                One Day project to recreate a website mockup from a Dribbble
+                Shot
+              </p>
+              <ButtonContainer>
+                <SmBtn
+                  href="https://dribbblechallenge-realestate.netlify.com/"
+                  as="a">
+                  Live Demo
+                </SmBtn>
+                <SmBtnOutline
+                  as="a"
+                  href="https://github.com/AndreBClark/dribbble-realestate">
+                  Github Repo
+                </SmBtnOutline>
+              </ButtonContainer>
+              <hr />
+            </Description>
           </Item>
           <Item>
-          <Thumbnail
+            <Thumbnail
               fluid={dscvr.childImageSharp.fluid}
-              alt="dscvr project" className="w-full"/>
-          <Description>
-            <h2>DSCVR Portfolio Project</h2>
-            <p>
-              project to create College Portfolio Site for North Idaho College
-              Graphic Design Program
-            </p>
-            <ButtonContainer>
-              <SmBtn
-                as="a"
-                href="https://dscvr2019.netlify.com/"
-                target="_blank">
-                Live Demo
-              </SmBtn>
-              <SmBtnOutline
-                as="a"
-                href="https://github.com/AndreBClark/dscvr"
-                target="_blank">
-                Github Repo
-              </SmBtnOutline>
-            </ButtonContainer>
-            <hr />
-          </Description>
+              alt="dscvr project"
+              className="w-full"
+            />
+            <Description>
+              <h2>DSCVR Portfolio Project</h2>
+              <p>
+                project to create College Portfolio Site for North Idaho College
+                Graphic Design Program
+              </p>
+              <ButtonContainer>
+                <SmBtn
+                  as="a"
+                  href="https://dscvr2019.netlify.com/"
+                  target="_blank">
+                  Live Demo
+                </SmBtn>
+                <SmBtnOutline
+                  as="a"
+                  href="https://github.com/AndreBClark/dscvr"
+                  target="_blank">
+                  Github Repo
+                </SmBtnOutline>
+              </ButtonContainer>
+              <hr />
+            </Description>
           </Item>
         </Panel>
       </ContentContainer>
