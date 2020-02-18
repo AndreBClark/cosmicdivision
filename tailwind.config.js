08c02ef30a195e564219cec461d063b7c7ad9266
AndreBClark <andre@cosmicdivision.dev>
2020-02-17 22:58:46 -0800

    :sparkles: theme extend fontsizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -33,7 +33,13 @@ module.exports = {
                     ...defaultTheme.fontFamily.sans
                 ],
                 'heading': 'Orbitron',
-            }
+            },
+            fontSize: {
+                "7xl": "5rem",
+                "8xl": "6rem",
+                "9xl": "7rem",
+                "10xl": "8rem"
+            },
         }
     },
     variants: {},
