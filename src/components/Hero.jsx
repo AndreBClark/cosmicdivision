acc6a0d7804aa1d6f1ea78a9d8fe86c352d6c032
AndreBClark <andre@cosmicdivision.dev>
2021-07-12 15:56:28 -0700

    :truck: move components out of pages

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
-import styled from 'styled-components'
-import theme from '../../config/theme'
+import tw, { styled } from 'twin.macro'
+import { breakpoints,  fontFamily, colors } from '../../config/theme'
+import { rhythm } from '../../config/typography'
 import ContentContainer from './ContentContainer'
 import BackgroundImage from 'gatsby-background-image'
 
 const Container = styled(BackgroundImage)`
-  background-color: ${theme.colors.grey.dark};
+  background-color: ${colors.grey.dark};
   width: 100%;
   min-height: 75vh;
   text-align: center;
@@ -15,39 +16,69 @@ const Container = styled(BackgroundImage)`
   background-position: center;
   display: flex;
   align-items: center;
-  @media screen and (min-width: 769px) {
+  @media screen and (min-width: ${breakpoints.tablet}) {
     min-height: 80vh;
   }
 `
 
 const Overlay = styled.div`
-    content: '';
-    position: absolute;
-    height: 50vh;
-    z-index: -1;
-    width: 100%;
-    background-image: linear-gradient(0deg,rgb(18, 18, 18) 15%,rgba(18,18,18,0) 50%);
+  content: '';
+  position: absolute;
+  height: 50vh;
+  bottom: 0;
+  z-index: -1;
+  width: 100%;
+  background-image: linear-gradient(
+    0deg,
+    rgb(18, 18, 18) 15%,
+    rgba(18, 18, 18, 0) 50%
+  );
 `
 const FlexLg = styled.div`
   display: flex;
   align-content: center;
 
-  @media screen and (min-width: ${props => props.theme.breakpoints.tablet}) {
+  @media screen and (min-width: ${breakpoints.tablet}) {
     justify-content: space-between;
   }
+`
+
+export const HeroInner = styled.div`
+  text-align: center;
+  margin: 0;
+  @media screen and (min-width: ${breakpoints.tablet}) {
+    margin-left: auto;
+    margin-right: auto;
+  }
+`
+export const HeadingOne = styled.h1`
+  ${tw`text-5xl`}
+  text-transform: ${fontFamily.logoCase};
+`
+export const Subtitle = styled.h2`
+${tw`text-lg`}
+  font-family: ${fontFamily.heading};
+  text-transform: ${fontFamily.headingCase};
+  font-weight: semibold;
+  border-radius: ${rhythm(1 / 8)};
+  margin: ${rhythm(1 / 4)};
+  padding: ${rhythm(1 / 8)} ${rhythm(1)};
+  color: ${colors.bg};
+  background-color: ${colors.primary};
+  font-weight: ${fontFamily.headingWeight};
 `
 const Hero = ({ children }) => {
   const { mobileImage, desktopImage } = useStaticQuery(
     graphql`
       query {
-        mobileImage: file(relativePath: {eq: "orbital-bw.jpg"}) {
-        childImageSharp {
+        mobileImage: file(relativePath: { eq: "orbital-bw.jpg" }) {
+          childImageSharp {
             fluid(quality: 100, maxWidth: 412) {
               ...GatsbyImageSharpFluid_withWebp
             }
           }
-      }
-      desktopImage: file(relativePath: { eq: "orbital-bw.jpg" }) {
+        }
+        desktopImage: file(relativePath: { eq: "orbital-bw.jpg" }) {
           childImageSharp {
             fluid(quality: 75, maxWidth: 1920) {
               ...GatsbyImageSharpFluid_withWebp
@@ -65,16 +96,15 @@ const Hero = ({ children }) => {
     },
   ]
 
-      return (
-        <Container Tag="section" fluid={sources} backgroundColor={`#040e18`}>
-            <Overlay> </Overlay>
-          <ContentContainer>
-            <FlexLg>
-              {children}
-            </FlexLg>
-          </ContentContainer>
-        </Container>
-      )
+  return (
+    <Container Tag="section" fluid={sources} backgroundColor={`#040e18`}>
+      <Overlay> </Overlay>
+      <ContentContainer>
+        <FlexLg>{children}</FlexLg>
+      </ContentContainer>
+    </Container>
+  )
 }
+
 
 export default Hero
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,80 +1,73 @@
 import React from 'react'
 import ContentContainer from '../components/ContentContainer'
 import BioCard from '../components/BioCard'
-import styled from 'styled-components'
-import tw from 'tailwind.macro'
 import { BtnOutline, Btn } from '../components/button'
 import Panel from '../components/Panel'
 import PageHeading from '../components/PageHeading'
-import { Subtitle } from '.'
-
-const TextInput = styled.input`
-  ${tw`block p-2 text-white bg-gray-300 border-2 border-gray-300 rounded-lg w-72`}
-  :hover,
-  :focus {
-    ${tw`bg-gray-600 border-primary`}
-  }
-`
-const Label = styled.label`
-  ${tw`block my-2`}
-`
-export const DualWrapper = styled.div`
-`
+import { Subtitle } from '../components/Hero'
+import { Label, TextInput, DualWrapper } from '../components/TextInput'
+import Seo from '../components/Seo'
 
 const Contact = () => {
-    return (
-    <ContentContainer>
-      <PageHeading>Contact Me</PageHeading>
-      <Subtitle>Get in touch with me</Subtitle>
-      <BioCard />
-      <Panel>
-        <form method="post" netlify-honeypot="bot-field" data-netlify="true">
-          <input type="hidden" name="bot-field" />
-          <Label>
-            Full Name
-            <TextInput
-              type="text"
-              name="name"
-              id="name"
-              placeholder="First Last"
-            />
-          </Label>
-          <Label>
-            Email Address
-            <TextInput
-              type="email"
-              name="email"
-              id="email"
-              placeholder="name@example.com"
-            />
-          </Label>
-          <label>
-            Subject
-            <TextInput
-              type="text"
-              name="subject"
-              id="subject"
-              placeholder="Subject"
-            />
-          </label>
-          <Label>
-            Message
-            <TextInput
-              name="message"
-              id="message"
-              rows="5"
-              placeholder="Message"
-            />
-          </Label>
-          <DualWrapper>
-            <Btn type="submit">Send</Btn>
-            <BtnOutline type="reset" value="Clear">
-              Reset
-            </BtnOutline>
-          </DualWrapper>
-        </form>
-      </Panel>
-    </ContentContainer>
+  return (
+    <>
+      <Seo
+        title={'Contact'}
+        keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+      />
+      <ContentContainer>
+        <PageHeading>Contact Me</PageHeading>
+        <Subtitle>Get in touch with me</Subtitle>
+        <BioCard />
+        <Panel>
+          <form method="post" netlify-honeypot="bot-field" data-netlify="true">
+            <input type="hidden" name="bot-field" />
+            <Label>
+              Full Name
+              <TextInput
+                type="text"
+                name="name"
+                id="name"
+                placeholder="First Last"
+              />
+            </Label>
+            <Label>
+              Email Address
+              <TextInput
+                type="email"
+                name="email"
+                id="email"
+                placeholder="name@example.com"
+              />
+            </Label>
+            <Label>
+              Subject
+              <TextInput
+                type="text"
+                name="subject"
+                id="subject"
+                placeholder="Subject"
+              />
+            </Label>
+            <Label>
+              Message
+              <TextInput
+                name="message"
+                id="message"
+                rows="5"
+                placeholder="Message"
+              />
+            </Label>
+            <DualWrapper>
+              <Btn type="submit">Send</Btn>
+              <BtnOutline type="reset" value="Clear">
+                Reset
+              </BtnOutline>
+            </DualWrapper>
+          </form>
+        </Panel>
+      </ContentContainer>
+    </>
   )
 }
 export default Contact
