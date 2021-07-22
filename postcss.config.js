1f5c8964521afc862d9cd9ea96d885cc753e9681
AndreBClark <andre@cosmicdivision.dev>
2021-07-21 18:04:28 -0700

    :fire: remove postcss plugins

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,7 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-    content: [
-        './src/**/*.jsx',
-    ],
-    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-});
-const cssnano = require('cssnano')('cssnano-advanced-preset');
-
-module.exports = () => ({
-    plugins: [
-        require('precss'),
-        require('tailwindcss'),
-        ...process.env.NODE_ENV === 'production' ? [purgecss, cssnano] : []
-    ]
-})
+module.exports = {
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+        cssnano: {},
+    },
+}
