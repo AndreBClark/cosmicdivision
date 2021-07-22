e61dfdc9ee063555ff0f0d590067647a8580d0a6
AndreBClark <andre@cosmicdivision.dev>
2021-07-21 17:58:23 -0700

    :sparkles: add MastHead, for unified page heading, subtitle, SEO components

---
+++ src/components/MastHead.jsx
@@ -0,0 +1,45 @@
+import tw from 'twin.macro'
+import React from 'react'
+import Seo from 'components/Seo'
+
+
+const MastHead = ({
+  pagetitle,
+  pageKeywords = [`blog`, `gatsby`, `javascript`, `react`],
+  pageHeading,
+  pageSubtitle,
+  pageDate,
+  location
+}) => {
+  return (
+    <>
+      <Seo
+        location={location}
+        title={pagetitle}
+        keywords={pageKeywords}
+      />
+      {pageHeading && <PageHeading>{pageHeading}</PageHeading>}
+      <div
+        tw="bg-primary w-full"
+      >
+        {pageSubtitle && <Subtitle>{pageSubtitle}</Subtitle>}
+        {pageDate && <Date>{pageDate}</Date>}
+      </div>
+    </>
+  )
+}
+
+
+const PageHeading = tw.h1`mx-auto px-4 my-8 max-w-screen-xl`
+
+const Date = tw.p`m-8 max-w-screen-xl`
+
+
+const Subtitle = tw.h2`
+  mx-auto block max-w-7xl px-8 text-lg font-heading capitalize font-semibold text-gray-800
+  lg:(tracking-widest text-2xl px-4)
+
+`
+
+
+export default MastHead;
