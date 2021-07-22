8a959b22c2f9e9d11b8557157192f199411cbd1d
AndreBClark <andre@cosmicdivision.dev>
2021-07-21 18:11:58 -0700

    :fire: tailwind config remove all polished functions

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,17 +14,7 @@ module.exports = {
             colors: {
                 primary: '#26ffba',
                 gray: {
-                    900: shade(.7, "#121212"),
-                    800: shade(.5, "#121212"),
-                    700: shade(.4, "#121212"),
-                    600: shade(.3, "#121212"),
-                    500: '#121212',
-                    400: tint(0.1, "#121212"),
-                    300: tint(0.2, "#121212"),
-                    200: tint(0.3, "#121212"),
-                    100: tint(0.4, "#121212")
-                }
-            },
+                    50: colors.gray[50],
             fontFamily: {
                 'mono': `Fira Mono`,
                 'sans': [
