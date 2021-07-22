8491bea8840ff80afadde4680a13cd447b5b94f8
AndreBClark <andre@cosmicdivision.dev>
2021-07-21 18:10:10 -0700

    :fire: blog-post template remove comments

--- src/templates/blog-post.jsx
+++ src/templates/blog-post.jsx
@@ -8,61 +8,23 @@ const BlogPostTemplate = ({ location, data }) => {
   const post = data.markdownRemark;
   return (
     <PageContained
-        location={location}
+      location={location}
       pageHeading={post.frontmatter.title}
       pageSubtitle={post.frontmatter.description || post.excerpt}
       date={post.frontmatter.date}
     >
-        <Panel>
-          {/* <TinaField name="rawMarkdownBody" Component={Wysiwyg}> */}
-          <Article dangerouslySetInnerHTML={{ __html: post.html }} />
-          {/* </TinaField>
-          {process.env.NODE_ENV !== 'production' && (
-            <Btn onClick={() => setIsEditing(p => !p)}>
-              {isEditing ? 'Preview' : 'Edit'}
-            </Btn>
-          )} */}
-        </Panel>
-      </ContentContainer>
-    </>
+      <Panel>
+        <article
+          tw="prose xl:prose-2xl"
+          dangerouslySetInnerHTML={{ __html: post.html }} />
+      </Panel>
+    </PageContained>
   )
 }
-// const BlogTemplateOptions = {
-//   fields: [
-//     {
-//       label: 'Title',
-//       name: 'rawFrontmatter.title',
-//       component: 'text',
-//     },
-//     {
-//       name: 'frontmatter.draft',
-//       component: 'toggle',
-//       label: 'Draft',
-//     },
-//     {
-//       label: 'Date Posted',
-//       name: 'rawFrontmatter.date',
-//       component: 'date',
-//       dateFormat: 'MMMM DD YYYY',
-//       timeFormat: false,
-//     },
-//     {
-//       label: 'Body',
-//       name: 'rawMarkdownBody',
-//       component: 'markdown',
-//     },
-//   ],
-// }
 
-export default BlogPostTemplate
 
 export const pageQuery = graphql`
   query BlogPostBySlug($slug: String!) {
-    site {
-      siteMetadata {
-        title
-      }
-    }
     markdownRemark(fields: { slug: { eq: $slug } }) {
       id
       excerpt(pruneLength: 160)
@@ -75,3 +37,6 @@ export const pageQuery = graphql`
     }
   }
 `
+
+
+export default BlogPostTemplate
