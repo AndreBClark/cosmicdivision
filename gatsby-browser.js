44a98f08a6b6c7cc5072d66c2874dfbecfc7aef2
Andre <andre@cosmicdivision.dev>
2021-07-28 14:43:24 -0700

    :truck: move browser imports

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,8 @@
+import "./static/preflight.min.css"
+import '@fontsource/fira-sans'
 import '@fontsource/orbitron'
 import '@fontsource/orbitron/700.css'
-import '@fontsource/fira-code'
-import '@fontsource/fira-sans'
+import '@fontsource/orbitron/900.css'
 import '@fontsource/fira-sans/700.css'
+import '@fontsource/fira-code'
 import './static/prism-duotone-sea.min.css'
-import "./static/preflight.min.css"
