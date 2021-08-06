7f61ec682cda7dfe1e66ec4aab4976ca47762088
Andre <andre@cosmicdivision.dev>
2021-08-06 16:09:12 -0700

    :sparkle: add schema types from sanity template

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -5,10 +5,19 @@ import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 // We import object and document schemas
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
+import blockContent from './documents/blockContent'
+
+// document schemas
+import author from "./documents/author";
+import category from "./documents/category";
+import post from "./documents/post";
+import siteSettings from "./documents/siteSettings";
+// Object types
+import bodyPortableText from "./objects/bodyPortableText";
+import bioPortableText from "./objects/bioPortableText";
+import excerptPortableText from "./objects/excerptPortableText";
+import mainImage from "./objects/mainImage";
+import authorReference from "./objects/authorReference";
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -19,11 +28,14 @@ export default createSchema({
   types: schemaTypes.concat([
     // The following are document types which will appear
     // in the studio.
+    siteSettings,
     post,
-    author,
     category,
-    // When added to this list, object types can be used as
-    // { type: 'typename' } in other document schemas
-    blockContent,
+    author,
+    mainImage,
+    authorReference,
+    bodyPortableText,
+    bioPortableText,
+    excerptPortableText,
   ]),
 })
