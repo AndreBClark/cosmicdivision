8035f5c3863eb8f11b1144fe106b07d28578b8b1
Andre <andre@cosmicdivision.dev>
2021-08-06 16:07:15 -0700

    add client-config file

---
+++ client-config.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+  sanity: {
+    projectId:
+      process.env.GATSBY_SANITY_PROJECT_ID || "<#< sanity.projectId >#>",
+    dataset: process.env.GATSBY_SANITY_DATASET || "<#< sanity.dataset >#>",
+  },
+};
