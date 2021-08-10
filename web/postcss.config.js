0e369f2144d76eb7898fdab701ee0327c7cf606e
Andre <andre@cosmicdivision.dev>
2021-08-10 16:42:56 -0700

    :art: minimize preflight css

--- web/postcss.config.js
+++ web/postcss.config.js
@@ -3,5 +3,16 @@ module.exports = {
         require('autoprefixer'),
         require('tailwindcss'),
         require('tailwindcss/nesting')('postcss-nesting'),
+        require('cssnano')({
+            preset: 'default',
+            discardComments: {
+                removeAll: true,
+            },
+            autoprefixer: true,
+            reduceIdents: true,
+            mergeIdents: true,
+            discardUnused: true,
+            
+        })
     ]
 }
